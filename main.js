dbc32553ddaa2b24446d88df941987eee9372d15
westkiteS2 <seoyeon8513@gmail.com>
2025-03-24 11:35:02 +0900

    5th(new)

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById('time').innerHTML = new Date().toLocaleString()
 
 function pink() {
@@ -40,46 +39,3 @@ function hide() {
   document.getElementById('fig').src = ''
   document.getElementById('desc').innerHTML = ''
 }
-=======
-document.getElementById('time').innerHTML = new Date().toLocaleString()
-
-function pink() {
-  document.body.style.backgroundColor = 'pink'
-}
-
-function yellow() {
-  document.body.style.backgroundColor = 'yellow'
-}
-
-function lightBlue() {
-  document.body.style.backgroundColor = 'lightblue'
-}
-
-function reset() {
-  document.body.style.backgroundColor = 'white'
-}
-
-function showhtml() {
-  document.getElementById('fig').src = 'img/HTML.png'
-  document.getElementById('desc').innerHTML =
-    '<b>HTML5</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
-}
-
-function showcss() {
-  document.getElementById('fig').src = 'img/CSS.png'
-  document.getElementById('desc').innerHTML =
-    '<b>CSS</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다다'
-}
-
-function showjs() {
-  document.getElementById('fig').src = 'img/Java.png'
-  document.getElementById('desc').innerHTML =
-    '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 설명할 수 있게 한다다'
-}
-/*다시*/
-
-function hide() {
-  document.getElementById('fig').src = ''
-  document.getElementById('desc').innerHTML = ''
-}
->>>>>>> a9f4e4b8168c7d3d197cfa976312f33a1b5db14e

e57e774dc3d9626ed42ed0843a8f460aea69dc0d
westkiteS2 <seoyeon8513@gmail.com>
2025-03-28 13:32:38 +0900

    3/28 1st

--- main.js
+++ main.js
@@ -17,23 +17,22 @@ function reset() {
 }
 
 function showhtml() {
-  document.getElementById('fig').src = 'img/HTML.png'
+  document.getElementById('fig').src = '/img/HTML.png'
   document.getElementById('desc').innerHTML =
     '<b>HTML5</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
 }
 
 function showcss() {
-  document.getElementById('fig').src = 'img/CSS.png'
+  document.getElementById('fig').src = '/img/CSS.png'
   document.getElementById('desc').innerHTML =
-    '<b>CSS</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다다'
+    '<b>CSS</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다'
 }
 
 function showjs() {
-  document.getElementById('fig').src = 'img/Java.png'
+  document.getElementById('fig').src = '/img/Java.png'
   document.getElementById('desc').innerHTML =
-    '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 설명할 수 있게 한다다'
+    '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 설명할 수 있게 한다'
 }
-/*다시*/
 
 function hide() {
   document.getElementById('fig').src = ''
